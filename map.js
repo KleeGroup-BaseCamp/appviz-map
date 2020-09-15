c51266da1de85564b2bdf9a0fe2bdf7df78957c5
farouk <faiz@eurecom.fr>
2020-09-15 10:57:30 +0200

    Extract domains from notebook

--- map.js
+++ map.js
@@ -2,6 +2,7 @@ class Map {
   constructor(state, config, notebook) {
     this.layers = initLayers(); // For testing purposes only
     let level = 0;
+    let domains = this.extractDomains(notebook);
     for (let layer of this.layers) {
       layer.level = level;
       layer.initStyle();
@@ -43,5 +44,28 @@ class Map {
       this.layers.forEach((layer) => layer.initStyle());
       document.querySelector("main").style.cursor = "default";
     }
+  }
+
+  extractDomains(notebook) {
+    let domains = {};
+    Object.keys(notebook).map((sketchName) => {
+      if (sketchName.slice(0, 2) === "Dt") {
+        // TO DO : Use a function updateDomains() to simplify code
+        let domainName = notebook[sketchName].packageName.split(".")[2];
+        if (domains[domainName]) {
+          if (domains[domainName].objects)
+            domains[domainName].objects.push(sketchName);
+          else domains[domainName].objects = [sketchName];
+        } else domains[domainName] = { objects: [sketchName] };
+      } else if (sketchName.slice(0, 2) === "Tk") {
+        let domainName = notebook[sketchName].packageName.split(".")[2];
+        if (domains[domainName]) {
+          if (domains[domainName].tasks)
+            domains[domainName].tasks.push(sketchName);
+          else domains[domainName].tasks = [sketchName];
+        } else domains[domainName] = { tasks: [sketchName] };
+      }
+    });
+    return domains;
   }
 }
