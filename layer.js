e3fb261f5e2b4c10c2845fa12306e987b723c9e8
phlppchrtn <philippe.chretien@kleegroup.com>
2020-09-15 14:35:45 +0200

    + simple

--- layer.js
+++ layer.js
@@ -27,12 +27,13 @@ class Layer {
     const rowSize = windowHeight / this.rows;
     const columnSize = windowWidth / this.columns;
     for (let i = 0; i < this.rows; i++) {
-      line(0, rowSize * (i + 1), windowWidth, rowSize * (i + 1));
+      line(0, rowSize*i, windowWidth, rowSize*i);
     }
     for (let j = 0; j < this.columns; j++) {
-      line(columnSize * (j + 1), 0, columnSize * (j + 1), windowHeight);
+      line(columnSize*j, 0, columnSize*j, windowHeight);
     }
   }
+
   initStyle() {
     this.elements.forEach((element) => element.initStyle());
   }
