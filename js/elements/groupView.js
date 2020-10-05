bebfba0e7a5b60b7ce952f7a4cec32efeaa32f17
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-05 22:20:49 +0200

    NO DUPLICATE CODE !!!!!!!!!!!!

--- js/elements/groupView.js
+++ js/elements/groupView.js
@@ -2,7 +2,7 @@ class GroupView extends Rectangle {
     
     constructor(width, height, title) {
         super(width, height);
-        this.title = title;
+        this.title = title ? Utils.buildDisplayableTitle(title, width) :  "No title";
     }
 
     render() {
@@ -22,35 +22,11 @@ class GroupView extends Rectangle {
         textSize(style.getFontSize("xxl"))
         textFont(style.getFont(false))
         textAlign(CENTER);
-        text(
-            this.title ? this.#getDisplayableTitle() : "No title",
-            0,
-            textAscent() + 15,
-            this._width
-        );
+        text(this.title, 0, textAscent() + 15, this._width);
     }
 
     #renderBar() {
         stroke(255)
         line(this._width / 4, textAscent() + textDescent() + 20, this._width * 3 / 4, textAscent() + textDescent() + 20)
-    }
-
-    #getMaxCharacters() {
-        let numOfCharacters = 1;
-        while (
-            numOfCharacters < this.title.length &&
-            textWidth(this.title.slice(0, numOfCharacters)) <
-            this._width - textWidth("m")
-        ) {
-            numOfCharacters++;
-        }
-        return numOfCharacters;
-    }
-
-    #getDisplayableTitle() {
-        const numOfCharacters = this.#getMaxCharacters();
-        return numOfCharacters == this.title.length ?
-            this.title :
-            this.title.slice(0, numOfCharacters - 3) + "...";
     }
 }
