8208b40c4ce2c4fb585399cf6a8649a36ec285e6
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-08 19:37:38 +0200

    Header !

--- js/views/group/itemTypeDetail.js
+++ js/views/group/itemTypeDetail.js
@@ -1,19 +1,19 @@
 class ItemTypeDetail extends Element {
     #width
     #height
-    title
+    #header
 
     constructor(width, height, title) {
         super()
         this.#width = width
         this.#height = height
+        this.#header = new Header(title, width, "l"/*fontSize*/, 255)
         this.title = this.title ? Utils.buildDisplayableTitle(this.title, this.#width, style.getFontSize("l")) : "No title"
     }
 
     render() {
         this.#renderRectangle()
-        this.#renderTitle()
-        this.#renderBar()
+        this.#header.render()
     }
 
     #renderRectangle() {
@@ -21,19 +21,5 @@ class ItemTypeDetail extends Element {
         stroke(255)
         fill([100, 100, 200])
         rect(0, 0, this.#width, this.#height)
-    }
-
-    #renderTitle() {
-        noStroke()
-        fill(style.getTextFill());
-        textSize(style.getFontSize("l"))
-        textFont(style.getFont(true))
-        textAlign(CENTER);
-        text(this.title, 0, textAscent() + 15, this.#width)
-    }
-
-    #renderBar() {
-        stroke(255)
-        line(this.#width / 4, textAscent() + textDescent() + 20, this.#width * 3 / 4, textAscent() + textDescent() + 20)
     }
 }
