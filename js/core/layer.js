7a0ea9d7b346df39718068e561b870ca7ab171c8
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-08 20:28:16 +0200

    code style

--- js/core/layer.js
+++ js/core/layer.js
@@ -14,19 +14,19 @@ class Layer {
   render() {
     for (let positionedElement of this.#positionedElements) {
       push();
-      translate(positionedElement.position.x, positionedElement.position.y);
-      positionedElement.element.render();
-      pop();
+      translate(positionedElement.position.x, positionedElement.position.y)
+      positionedElement.element.render()
+      pop()
     }
   }
 
   findElement(x, y) {
     for (let positionedElement of this.#positionedElements) {
-      const lx = x - positionedElement.position.x;
-      const ly = y - positionedElement.position.y;
+      const lx = x - positionedElement.position.x
+      const ly = y - positionedElement.position.y
       if (positionedElement.element.contains( lx, ly))
-        return positionedElement.element;
+        return positionedElement.element
     }
-    return null;
+    return null
   }
 }
