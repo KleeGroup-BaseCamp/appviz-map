0f9575d7c602b16095b1b62e5babbf3b454ef19c
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-19 11:35:33 +0200

    fix

--- js/views/demoView.js
+++ js/views/demoView.js
@@ -3,7 +3,7 @@ class DemoView   {
     provideLayers(modelRepository, layout) {
         return  [
             new LayerBuilder()
-            .addElement(new Card("demo_main", projection.getGridWidth(), projection.getGridHeight(), "Démo"))
+            .addElement(new Card("demo_main", projection.getPxSize(), "Démo"))
             .build()
         ]
     }
