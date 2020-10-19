04ac49daa8d2c0282a5f537450bece56f5e44dc9
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-19 12:51:27 +0200

    doc

--- js/layout/pxPosition.js
+++ js/layout/pxPosition.js
@@ -2,15 +2,27 @@ class PxPosition{
     #x
     #y
 
+    /**
+     * @constructor
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
     constructor(x, y){
         this.#x = x
         this.#y = y
     }
 
+    /**
+     * @return {number} x position
+     */
     getX(){
         return this.#x
     }
 
+    /**
+     * @return {number} y position
+     */
     getY(){
         return this.#y
     }
