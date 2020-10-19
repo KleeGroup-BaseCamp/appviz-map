13f42f32509bde256be25d1dcaffea3648928116
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-19 16:49:28 +0200

    Size and POsition are Vectors

--- js/layout/gridPosition.js
+++ js/layout/gridPosition.js
@@ -1,38 +1,15 @@
-class GridPosition{
-    /**
-     * Array.<string>
-     */
-    #columns
-    #rows
-    #length
+class GridPosition extends GridVector{
 
     constructor(columnCode, rowCode){
-        this.#columns = columnCode.split(":").map (s=> Number(s))
-        this.#rows = rowCode.split(":").map (s=> Number(s))
-        //---
-        if (this.#columns.length !== this.#rows.length){
-            throw 'levels of columns and rows mus be equal'    
-        }
-        this.#length = this.#columns.length //= this.#rows.length
+        super(columnCode, rowCode)
     }    
 
-    getLength(){
-        return this.#length
-    }
-
-    #checkLevel(level){
-        if (level<0 || level >= this.#length) { 
-            throw 'level must be between [0 ; '+this.#length +'['    
-        }
-    }
     
     getColumn(level){
-        this.#checkLevel(level)
-        return this.#columns[level]
+        return this._getColumns(level)
     }
 
     getRow(level){
-        this.#checkLevel(level)
-        return this.#rows[level]
+        return this._getRows(level)
     }
 }
--- js/layout/gridSize.js
+++ js/layout/gridSize.js
@@ -1,38 +1,14 @@
-class GridSize{
-    /**
-     * Array.<string>
-     */
-    #columns
-    #rows
-    #length
+class GridSize extends GridVector{
 
     constructor(columnCode, rowCode){
-        this.#columns = columnCode.split(":").map (s=> Number(s))
-        this.#rows = rowCode.split(":").map (s=> Number(s))
-        //---
-        if (this.#columns.length !== this.#rows.length){
-            throw 'levels of columns and rows mus be equal'    
-        }
-        this.#length = this.#columns.length //= this.#rows.length
+        super(columnCode, rowCode)
     }    
-
-    getLength(){
-        return this.#length
-    }
-
-    #checkLevel(level){
-        if (level<0 || level >= this.#length) { 
-            throw 'level must be between [0 ; '+this.#length +'['    
-        }
-    }
     
     getColumns(level){
-        this.#checkLevel(level)
-        return this.#columns[level]
+       return this._getColumns(level)
     }
 
     getRows(level){
-        this.#checkLevel(level)
-        return this.#rows[level]
+        return this._getRows(level)
     }
 }
