a9f1d2dd81a0a27246a1918925fb45edf39adce6
phlppchrtn <philippe.chretien@kleegroup.com>
2020-10-30 20:22:08 +0100

    clean

--- js/core/styleBuilder.ts
+++ js/core/styleBuilder.ts
@@ -9,6 +9,7 @@ export class StyleBuilder {
         const textFont = loadFont("fonts/Montserrat-Regular.ttf")
         const iconFont = loadFont("fonts/material-design-outlined.ttf")
         
+        /* DarkTheme */
         return  {
              icon: {
                 font : iconFont, 
@@ -53,41 +54,13 @@ export class StyleBuilder {
        }
     } 
 }
-
-//    private loadDarkTheme(): void {
-//        this.color.a = color("#2196F3") /* blue */
-//        this.color.b  = color( "#4CAF50") /* green */
-//        this.color.c  = color("#F44336") /* red */
-//        this.color.d  = color("#7881A9") /* grey */
-
-        /* back to front colors */
-//        this.color.back   = color("#2a2c3b")  /* deep dark */
-//        this.color.middle  = color("#3A3E52")  /* dark */
-//        this.color.front   = color("#505464")  /* light dark */
-
-        /* text */
-//        this.text.color.primary   = color("#FFFFF") /* white */ 
-//        this.text.color.secondary  = color("#9EA3B4")  /* light grey */
-
-//        this.color.undefined  = color("#fff700")  /* lemon*/
-//    }
-
-//    private loadLightTheme(): void {
-//        this.color.a = color("#2196F3") /* blue */
-//        this.color.b  = color( "#4CAF50") /* green */
-//        this.color.c  = color("#F44336") /* red */
-//        this.color.d  = color("#7881A9") /* grey */
-
+//        /* LightTheme */
 //        /* back to front colors */
 //        this.color.back   = color("#F6F6F4")  /*  light++ */
 //        this.color.middle  = color("#EFEFEF")  /* light   */
 //        this.color.front   = color("#DDDDDD")  /* light-- */
-       /* text */
+//        /* text */
 //        this.text.color.primary   = color("#000000") /* black */
 //        this.text.color.secondary  = color("#615c4b")
 //        this.color.undefined  = color("0008ff")
-//    }
-//    public getIcon(itemTypeName: ItemTypeName): string {
-//        return this.icons[itemTypeName]
-//    }
-//}
+
