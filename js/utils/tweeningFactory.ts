a7fee5495b1a8ef797727152e0b0fba576438c55
phlppchrtn <philippe.chretien@kleegroup.com>
2020-11-09 12:50:32 +0100

    easeOutElastic

--- js/utils/tweeningFactory.ts
+++ js/utils/tweeningFactory.ts
@@ -9,10 +9,20 @@ export class TweeningFactory {
             do : (r) => r
         }
     }
-    
+
     public easeOutSine ():Tweening {
         return {
             do : (r) => Math.sin(r * (Math.PI/2))
         }
     }
-}
+
+    public easeOutElastic(): Tweening {
+        return {
+            do : (r) => {
+        const c4 = (2 * Math.PI) / 3;
+        
+        return Math.pow(2, -10 * r) * Math.sin((r * 10 - 0.75) * c4) + 1;
+        }
+        }
+    }
+}   
