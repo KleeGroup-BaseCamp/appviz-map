2ed9c205794538aff0dc1ba81cf24f9fe0a3906f
farouk <farouk.faiz@kleegroup.com>
2020-11-09 16:16:04 +0100

    Animate light Fade in

--- js/incubator/light.ts
+++ js/incubator/light.ts
@@ -1,27 +1,30 @@
 import * as p5 from "p5";
 import { VElement } from "../core";
 import { PxPosition, PxSize } from "../layout";
+import {AnimationUtils} from "../utils"
 
 export class Light extends VElement{
-    private color : p5.Color
-    private readonly intensity: number
+    private readonly color : p5.Color
+    private value: number
     private readonly radius: number
     private readonly centerPosition: PxPosition
 
-    constructor(id: any, pxSize: PxSize, color: p5.Color, intensity: number){
+    constructor(id: any, pxSize: PxSize, color: p5.Color, value: number){ // value -> intensity ?
         super(id, pxSize, false)
         this.color = color
-        this.intensity = intensity
+        this.value = value
         this.radius = min(pxSize.getHeight(), pxSize.getWidth())
         this.centerPosition = new PxPosition(
             pxSize.getWidth() / 2, 
             pxSize.getHeight() / 2
             )
+        const duration = 1000 /*ms*/
+        AnimationUtils.animate(0, value, duration, (s:number) => this.value = s)
     }
 
     render(){
         noFill()
-        let maxOpacity = this.intensity * 255 / 100
+        let maxOpacity = this.value * 255 / 100
         for(let r = 0; r < this.radius; r++){
             let opacity = Math.round(maxOpacity* (1 - r / this.radius))
             this.color.setAlpha(opacity)
