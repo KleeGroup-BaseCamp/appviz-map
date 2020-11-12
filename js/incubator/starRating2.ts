c3b0a62b78b79231c4fa6cf601cdc8c0b31ceb73
phlppchrtn <philippe.chretien@kleegroup.com>
2020-11-12 15:00:45 +0100

    + simple

--- js/incubator/starRating2.ts
+++ js/incubator/starRating2.ts
@@ -4,10 +4,10 @@ import {style} from "../app"
 
 export class StarRating2 extends AbstractRating{
     private readonly corners: number = 5
+    private readonly angle = TWO_PI / this.corners
     
     public renderRatingIcon(size: number, value: number): void{
         noStroke()
-        const angle = TWO_PI / this.corners
         const externalRadius = size / 2
         const internalRadius = externalRadius / 2.5
 
@@ -24,25 +24,25 @@ export class StarRating2 extends AbstractRating{
         fill(style.color.front)
         if (value !=1 && value !=0.5){
             //only int or half-int are accepted 
-            this.renderStar(angle, externalRadius, internalRadius, true, true)
+            this.renderStar(externalRadius, internalRadius, true, true)
         }else if (value===0.5){
             //Half Star
-            this.renderStar(angle, internalRadius, internalRadius, false, true)
+            this.renderStar(internalRadius, internalRadius, false, true)
         }
         pop()
     }
 
-    private renderStar(angle: number, externalRadius: number, internalRadius: number, left: boolean, right: boolean): void {
+    private renderStar(externalRadius: number, internalRadius: number, left: boolean, right: boolean): void {
         beginShape()
         for (let i = 0; i <  this.corners; i++) {
             if (i == 0 || right && i <= 2 || left && i >= 3){
-                let sx = cos(angle * i) * externalRadius
-                let sy = sin(angle * i) * externalRadius
+                let sx = cos(this.angle * i) * externalRadius
+                let sy = sin(this.angle * i) * externalRadius
                 vertex(sx, sy)
             }
             if(right && i <= 2 || left && i >= 2){
-                let sx = cos(angle * (i + 1 / 2)) * internalRadius
-                let sy = sin(angle * (i + 1 / 2)) * internalRadius
+                let sx = cos(this.angle * (i + 1 / 2)) * internalRadius
+                let sy = sin(this.angle * (i + 1 / 2)) * internalRadius
                 vertex(sx, sy)
             }
         }
