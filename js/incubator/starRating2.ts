62898d75f69eea10ba74c0e2afd7e0062f119654
farouk <farouk.faiz@kleegroup.com>
2020-11-12 11:53:07 +0100

    Refactor StarRating2

--- js/incubator/starRating2.ts
+++ js/incubator/starRating2.ts
@@ -3,25 +3,24 @@ import {AbstractRating} from "./abstractRating"
 export class StarRating2 extends AbstractRating{
     
     public renderRatingIcon(size: number, value: number): void{
-        const ratio = 5 / 2
         noStroke()
         fill('gold')
-        this.star(0, 0, size / (2 * ratio), size / 2, 5);
-    }
+        const numOfCorners = 5
+        const angle = TWO_PI / numOfCorners
+        const offset = - PI / 2
+        const ratio = 5 / 2
+        const radius1 = size / 2
+        const radius2 = radius1 / ratio
 
-    private star(x: number, y: number, radius1: number, radius2: number, npoints: number) {
-        let angle = TWO_PI / npoints;
-        let halfAngle = angle / 2.0;
-        let b = - PI/2
-        beginShape();
+        beginShape()
         for (let a = 0; a < TWO_PI; a += angle) {
-            let sx = x + cos(a + b) * radius2;
-            let sy = y + sin(a + b) * radius2;
-            vertex(sx, sy);
-            sx = x + cos(a + b + halfAngle) * radius1;
-            sy = y + sin(a + b + halfAngle) * radius1;
-            vertex(sx, sy);
+            let sx = cos(offset + a) * radius1
+            let sy = sin(offset + a) * radius1
+            vertex(sx, sy)
+            sx = cos(offset + a + angle / 2) * radius2
+            sy = sin(offset + a + angle / 2) * radius2
+            vertex(sx, sy)
         }
-        endShape(CLOSE);
-        }
+        endShape(CLOSE)
+    }
 }
