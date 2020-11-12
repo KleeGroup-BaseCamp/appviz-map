6cc33355a06d07f5ad680bd8f98e03d6f69cf5be
phlppchrtn <philippe.chretien@kleegroup.com>
2020-11-12 16:49:38 +0100

    smooth

--- js/incubator/abstractRating.ts
+++ js/incubator/abstractRating.ts
@@ -3,21 +3,23 @@ import {PxSize} from "../layout";
 import {AnimationUtils} from "../utils"
 
 export abstract class AbstractRating extends VElement{
+    //How many icons are displayed ?
+    private readonly icons: number  = 5
     protected value: number
 
     constructor(id: any, pxSize: PxSize, value: number){
         super(id, pxSize, false)
         this.value = value
         const duration = 1000 /*ms*/
-        AnimationUtils.animate(0, value, duration, (s:number) => this.value = s)
+        AnimationUtils.animate(0, this.icons, duration, (s:number) => this.value = min(s,value) )
     }
 
     public render(){
         const margin = 3
-        const sideLength = min(this.getPxSize().getHeight(), this.getPxSize().getWidth() / 5 - margin)
+        const sideLength = min(this.getPxSize().getHeight(), this.getPxSize().getWidth() / this.icons - margin)
         let v = this.value
         push()
-        for(let i = 0; i < 5; i++){
+        for(let i = 0; i < this.icons; i++){
             this.renderRatingIcon(sideLength, min(v, 1))
             v = max(v - 1, 0)
             translate(sideLength + margin, 0)
