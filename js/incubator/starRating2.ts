30de8964c39d1b2050071a1e8bb2c4d4177c251d
phlppchrtn <philippe.chretien@kleegroup.com>
2020-11-12 12:19:10 +0100

    Revert "refac"
    This reverts commit 9ad1fc3fe7b62c3dc285a206b91d020968f2ae69.

--- js/incubator/starRating2.ts
+++ js/incubator/starRating2.ts
@@ -8,16 +8,17 @@ export class StarRating2 extends AbstractRating{
         const numOfCorners = 5
         const angle = TWO_PI / numOfCorners
         const offset = - PI / 2
-        const externalRadius = size / 2
-        const internalRadius  = externalRadius / (5/2)
+        const ratio = 5 / 2
+        const radius1 = size / 2
+        const radius2 = radius1 / ratio
 
         beginShape()
         for (let a = 0; a < TWO_PI; a += angle) {
-            let sx = cos(offset + a) * externalRadius
-            let sy = sin(offset + a) * externalRadius
+            let sx = cos(offset + a) * radius1
+            let sy = sin(offset + a) * radius1
             vertex(sx, sy)
-            sx = cos(offset + a + angle / 2) * internalRadius
-            sy = sin(offset + a + angle / 2) * internalRadius
+            sx = cos(offset + a + angle / 2) * radius2
+            sy = sin(offset + a + angle / 2) * radius2
             vertex(sx, sy)
         }
         endShape(CLOSE)
