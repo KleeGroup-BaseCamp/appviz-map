181b02f69c4319bd9e51294e82bc26ab8fa9169f
farouk <farouk.faiz@kleegroup.com>
2020-11-12 18:27:56 +0100

    Wave translation

--- js/incubator/loadingBarWithWaves.ts
+++ js/incubator/loadingBarWithWaves.ts
@@ -28,9 +28,7 @@ export class LoadingBarWithWaves extends VElement{
         rect(0, yFill, barWidth, barHeight - yFill)
 
         //Render wave
-        // To delete for test only
-        const diameter = barWidth / sqrt(2)
-        this.renderWave(yFill, barWidth, 0.35 * diameter)
+        this.renderWave(yFill, barWidth, 0.9 * barWidth)
 
         // Render Bar/container
         noFill()
@@ -41,7 +39,7 @@ export class LoadingBarWithWaves extends VElement{
      * 
      * @param yFill y coordinate of "liquid" surface
      * @param barWidth With of bar/container
-     * @param translationDistance in [0, barWidth] horizontal distance traveled by wave 
+     * @param translationDistance in [0, barWidth[ horizontal distance traveled by wave 
      */
     private renderWave(yFill: number, barWidth: number, translationDistance: number): void{
         push()
@@ -51,13 +49,39 @@ export class LoadingBarWithWaves extends VElement{
         // rect(0, sqrt(2) * diameter / 4, 300,100)
         noFill()
         
-        if (translationDistance < sqrt(2) * radius / 2){
+        if (translationDistance < barWidth / 4){
+            // Crop last arc
             translate(translationDistance, 0)
             this.renderArc(0, radius, 0, 0)
             this.renderArc(1, radius, 0, 0)
             this.renderArc(2, radius, asin(translationDistance / radius), 0)
             translate(- barWidth, 0)
-            this.renderArc(2, radius, 0, QUARTER_PI - asin(translationDistance / radius))
+            this.renderArc(2, radius, 0, - QUARTER_PI + asin(translationDistance / radius))
+        } else if (translationDistance < barWidth / 2){
+            // Crop second part of second arc
+            translate(translationDistance, 0)
+            this.renderArc(0, radius, 0, 0)
+            this.renderArc(1, radius, 0, - asin((translationDistance - barWidth / 4) / radius))
+            translate(- barWidth, 0)
+            this.renderArc(2, radius, 0, 0)
+            this.renderArc(1, radius, HALF_PI - asin((translationDistance - barWidth / 4) / radius), 0)
+        } else if (translationDistance < 3 * barWidth / 4){
+            // Crop first part of second arc
+            translate(translationDistance, 0)
+            this.renderArc(0, radius, 0, 0)
+            this.renderArc(1, radius, 0, - QUARTER_PI - asin((translationDistance - barWidth / 2) / radius))
+            translate(- barWidth, 0)
+            this.renderArc(2, radius, 0, 0)
+            this.renderArc(1, radius, QUARTER_PI - asin((translationDistance - barWidth / 2) / radius), 0)
+        } else{
+            // 3 * barWidth / 4 <= translationDistance < barWidth
+            // Crop first arc
+            translate(translationDistance, 0)
+            this.renderArc(0, radius, asin((translationDistance - 3 * barWidth / 4) / radius), 0)
+            translate(- barWidth, 0)
+            this.renderArc(0, radius, 0, - QUARTER_PI + asin((translationDistance - 3 * barWidth / 4) / radius))
+            this.renderArc(1, radius, 0, 0)
+            this.renderArc(2, radius, 0, 0)
         }
         pop()
 
