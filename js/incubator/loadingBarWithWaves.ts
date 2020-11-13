0a601a0f1d76d21f91b9ceec3eea0b8c32a57c2b
farouk <farouk.faiz@kleegroup.com>
2020-11-13 12:40:03 +0100

    Simple single wave (Bezier)

--- js/incubator/loadingBarWithWaves.ts
+++ js/incubator/loadingBarWithWaves.ts
@@ -7,15 +7,20 @@ export class LoadingBarWithWaves extends VElement{
 
 
     private value: number
-    private translationDistance: number
+    private maxAmplitude: number
+    private time: number
 
     constructor(id: any, pxSize: PxSize, value: number){ // value -> intensity ?
         super(id, pxSize, false)
+        // Initialise with some value
         this.value = value
-        this.translationDistance = 0
+        this.maxAmplitude = 0 
+        this.time = 0
+        
         const duration = 1000 /*ms*/
-        AnimationUtils.animate(0, 5 * pxSize.getWidth(), duration, (s:number) => this.translationDistance = s)
         AnimationUtils.animate(0, value, duration, (s:number) => this.value = s)
+        AnimationUtils.animate(30, 0, duration * 10, (s:number) => this.maxAmplitude = s)
+        AnimationUtils.animate(0, 100, duration * 10, (s:number) => this.time = s)
     }
 
     public render() : void {
@@ -31,7 +36,7 @@ export class LoadingBarWithWaves extends VElement{
         rect(0, yFill, barWidth, barHeight - yFill)
 
         //Render wave
-        this.renderWave(yFill, barWidth, this.translationDistance % barWidth)
+        this.renderWave(yFill, barWidth)
 
         // Render Bar/container
         noFill()
@@ -42,89 +47,25 @@ export class LoadingBarWithWaves extends VElement{
      * 
      * @param yFill y coordinate of "liquid" surface
      * @param barWidth With of bar/container
-     * @param translationDistance in [0, barWidth[ horizontal distance traveled by wave 
      */
-    private renderWave(yFill: number, barWidth: number, translationDistance: number): void{
+    private renderWave(yFill: number, barWidth: number): void{
         push()
         stroke(style.color.front)
-        const radius = barWidth / (2 * sqrt(2))
-        translate(0, yFill - radius)
-        // rect(0, sqrt(2) * diameter / 4, 300,100)
-        noFill()
-        
-        if (translationDistance < barWidth / 4){
-            // Crop last arc
-            translate(translationDistance, 0)
-            this.renderArc(0, radius, 0, 0)
-            this.renderArc(1, radius, 0, 0)
-            this.renderArc(2, radius, asin(translationDistance / radius), 0)
-            translate(- barWidth, 0)
-            this.renderArc(2, radius, 0, - QUARTER_PI + asin(translationDistance / radius))
-        } else if (translationDistance < barWidth / 2){
-            // Crop second part of second arc
-            translate(translationDistance, 0)
-            this.renderArc(0, radius, 0, 0)
-            this.renderArc(1, radius, 0, - asin((translationDistance - barWidth / 4) / radius))
-            translate(- barWidth, 0)
-            this.renderArc(2, radius, 0, 0)
-            this.renderArc(1, radius, HALF_PI - asin((translationDistance - barWidth / 4) / radius), 0)
-        } else if (translationDistance < 3 * barWidth / 4){
-            // Crop first part of second arc
-            translate(translationDistance, 0)
-            this.renderArc(0, radius, 0, 0)
-            this.renderArc(1, radius, 0, - QUARTER_PI - asin((translationDistance - barWidth / 2) / radius))
-            translate(- barWidth, 0)
-            this.renderArc(2, radius, 0, 0)
-            this.renderArc(1, radius, QUARTER_PI - asin((translationDistance - barWidth / 2) / radius), 0)
-        } else{
-            // 3 * barWidth / 4 <= translationDistance < barWidth
-            // Crop first arc
-            translate(translationDistance, 0)
-            this.renderArc(0, radius, asin((translationDistance - 3 * barWidth / 4) / radius), 0)
-            translate(- barWidth, 0)
-            this.renderArc(0, radius, 0, - QUARTER_PI + asin((translationDistance - 3 * barWidth / 4) / radius))
-            this.renderArc(1, radius, 0, 0)
-            this.renderArc(2, radius, 0, 0)
-        }
-        pop()
-
-
-        
-    }
-
-    private renderArc(arcIndex:number, radius: number, start: number, end: number){
-        switch(arcIndex){
-            case 0:
-                arc(
-                    0,
-                    0, 
-                    radius * 2, 
-                    radius * 2, 
-                    QUARTER_PI + start, 
-                    HALF_PI + end
-                )
-                break
-            case 1:
-                arc(
-                    radius * sqrt(2), 
-                    radius * sqrt(2), 
-                    radius * 2, 
-                    radius * 2, 
-                    PI + QUARTER_PI + start, 
-                    -QUARTER_PI + end
-                )
-                break
-            case 2:
-                arc(
-                radius * sqrt(2) *  2, 
-                0, 
-                radius * 2, 
-                radius * 2, 
-                HALF_PI + start, 
-                PI - QUARTER_PI + end
+        translate(0, yFill)
+        const period = 10
+        const fillHeight = this.getPxSize().getHeight() - yFill 
+        const amplitude = min(min(this.maxAmplitude, fillHeight), yFill) // bounding box constraints
+        beginShape()
+        vertex(0, 0)
+        bezierVertex(
+            barWidth / 3, 
+            - amplitude * sin(TWO_PI * this.time / period), 
+            barWidth * 2 / 3, 
+            - amplitude * sin(TWO_PI * this.time / period + HALF_PI), 
+            barWidth, 
+            0
             )
-        }
-        
+        endShape()
+        pop()
     }
-
 }
