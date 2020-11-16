0fb12e556ecac7129a702d66b795e82dd7e47666
phlppchrtn <philippe.chretien@kleegroup.com>
2020-11-16 23:21:19 +0100

    icons

--- js/views/demoViewBattery.ts
+++ js/views/demoViewBattery.ts
@@ -5,7 +5,7 @@ import {Card} from "./elements"
 import {ModelRepository} from "../model"
 import {Layout} from "../types"
 
-import {Battery} from "../incubator" 
+import {Battery, Light} from "../incubator" 
 import {PxSize} from "../layout"
 
 export class DemoViewBattery implements View {
@@ -17,9 +17,15 @@ export class DemoViewBattery implements View {
                 .addElement(new Card("demo_main", projection.getPxSize(), "Demo Battery"))
                 .build(),
             new GridLayerBuilder()
-                .addElement(new Battery("-1", pxSize, 10))
+                .addElement(new Battery("-1", pxSize, 15))
                 .addElement(new Battery("-1", pxSize, 50))
-                .addElement(new Battery("-1", pxSize, 90))
+                .addElement(new Battery("-1", pxSize, 75))
+                .addElement(new Battery("-1", pxSize, 100))
+                .beginRow()
+                .addElement(new Light("-1", pxSize, color(255, 225, 0), 25))
+                .addElement(new Light("-1", pxSize, color(255, 225, 0), 50))
+                .addElement(new Light("-1", pxSize, color(255, 225, 0), 75))
+                .addElement(new Light("-1", pxSize, color(255, 225, 0), 100))
                 .build()
         ]
     }
