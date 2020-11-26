8978871e9481d7e4f35608ba1a0dedda80ee85d5
farouk <farouk.faiz@kleegroup.com>
2020-11-26 17:15:46 +0100

    Neon circles animation (first version)

--- js/incubator/neonCircles.ts
+++ js/incubator/neonCircles.ts
@@ -1,16 +1,34 @@
-import * as p5 from "p5";
-import { sketch } from "../app";
-import { VElement } from "../core";
-import { PxSize } from "../layout";
+import * as p5 from "p5"
+import {VElement} from "../core"
+import {PxSize} from "../layout"
+import {AnimationUtils} from "../utils"
 
 declare let drawingContext: CanvasRenderingContext2D
 
 export class NeonCircles extends VElement{
-    private readonly primaryColor = color("HotPink")
-    private readonly secondaryColor = color("CornflowerBlue")
+    private readonly primaryColor = color("Violet")
+    private readonly secondaryColor = color("DeepSkyBlue")
+    private readonly circles: NeonCircle[] = []
 
     constructor(id: any, pxSize: PxSize){
         super(id, pxSize, false)
+        NeonCircle.primaryColor = this.primaryColor
+        NeonCircle.secondaryColor = this.secondaryColor
+        NeonCircle.maxRadius = min(this.getWidth(), this.getHeight()) / 2 * 0.8 // TO DO: estimate space taken by neon
+        const numOfCircles = 5
+        for(let i = 0; i < numOfCircles; i++){
+            this.circles.push(new NeonCircle((i + 1) / numOfCircles * NeonCircle.maxRadius))
+        }
+        AnimationUtils.animate(
+            0, 
+            100, 
+            2000, 
+            (s) => {
+                this.circles.forEach(
+                    circle => circle.update(s)
+                )
+            }
+        )
     }
 
     public render(): void{
@@ -18,11 +36,9 @@ export class NeonCircles extends VElement{
         strokeWeight(2)
         push()
         translate(this.getWidth() / 2, this.getHeight() / 2)
-        const numOfCircles = 5
-        const maxRadius = min(this.getWidth(), this.getHeight()) / 2
-        for(let i =0; i < numOfCircles; i++){
-            this.renderNeonCircle((i + 1) / numOfCircles * maxRadius, lerpColor(this.primaryColor, this.secondaryColor, i / numOfCircles))
-        }
+        this.circles.forEach(
+            circle => circle.render()
+        )
         pop()
     }
 
@@ -31,7 +47,48 @@ export class NeonCircles extends VElement{
         stroke(color)
         drawingContext.shadowColor = color.toString();
         drawingContext.shadowBlur = 10 // TO DO: try different values for best visual
-        circle(0, 0, radius)
+        circle(0, 0, 2 * radius)
         pop()
     }
+}
+
+class NeonCircle{
+    static primaryColor: p5.Color // TO DO: éviter de changer les fields static pour chaque NeonCircles composant
+    static secondaryColor: p5.Color
+    static maxRadius: number
+    private color: p5.Color = NeonCircle.primaryColor
+    private radius: number
+    private blur: number = 0
+
+    constructor(radius: number){
+        this.radius = radius
+    }
+
+    public render(): void{
+        push()
+        stroke(this.color)
+        drawingContext.shadowColor = this.color.toString();
+        drawingContext.shadowBlur = this.blur 
+        circle(0, 0, 2 * this.radius)
+        pop()
+    }
+
+    public update(percent: number): void{
+        const firstAnimEnd = 90
+        const maxBlur = 15 // TO DO: try different values for best visual
+        if(percent < firstAnimEnd){
+            // 0 -> 70 converge radius & increase opacity & blur
+            const finalRadius = NeonCircle.maxRadius / 2
+            const ratio = percent / firstAnimEnd
+            this.radius += ratio * (finalRadius - this.radius)
+            lerpColor(NeonCircle.primaryColor, NeonCircle.secondaryColor, this.radius / NeonCircle.maxRadius)
+            this.color.setAlpha(ratio * 255)
+            this.blur = ratio * maxBlur
+        } else{
+            // 70 -> 100 decrease blur
+            const ratio = (percent - firstAnimEnd) / (100 - firstAnimEnd)
+            this.blur = (1 - ratio) * maxBlur
+        }
+    }
+
 }
