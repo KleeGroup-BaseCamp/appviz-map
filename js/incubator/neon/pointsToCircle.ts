549c3ddaee20bf3a759aa3a35d4a64cf856bf01e
farouk <farouk.faiz@kleegroup.com>
2020-11-30 11:31:00 +0100

    Add circle in PointsToCircle

--- js/incubator/neon/pointsToCircle.ts
+++ js/incubator/neon/pointsToCircle.ts
@@ -1,20 +1,25 @@
+import * as p5 from "p5"
 import {VElement} from "../../core"
 import {PxSize} from "../../layout"
+import {Easings} from "../../utils/easings"
 import {AnimationUtils} from "../../utils"
 
 declare let drawingContext: CanvasRenderingContext2D // Duplicate (neonCircles) --> To declare globally
 
 export class PointsToCircle extends VElement{
     private readonly points: NeonPoint[] = []
+    private readonly neonCircle: NeonCircle
 
     constructor(id: any, pxSize: PxSize){
         super(id, pxSize, false)
         const numOfPoints = 30
         const maxRadius = min(this.getWidth(), this.getHeight()) / 2 * 0.8 // TO DO: estimate space taken by neon
         for(let i = 0; i < numOfPoints; i++){
-            this.points.push(new NeonPoint(maxRadius, random(TWO_PI), random(maxRadius)))
+            this.points.push(new NeonPoint(maxRadius, random(TWO_PI), random(maxRadius/2, maxRadius)))
         }
+        this.neonCircle = new NeonCircle(maxRadius, color("DeepSkyBlue"), 4, 15)
         AnimationUtils.animate(0, 100, 5000, s=>this.update(s))
+        AnimationUtils.animate(0, 100, 5000, s=>this.neonCircle.update(s), new Easings().easeInQuad)
     }
 
     private update(percent :number): void{
@@ -29,6 +34,7 @@ export class PointsToCircle extends VElement{
         this.points.forEach(
             point => point.render()
         )
+        this.neonCircle.render()
         pop()
     }
 }
@@ -41,6 +47,7 @@ class NeonPoint{
     private readonly startDistance: number
     private distance: number = 0
     private angle: number = 0
+    private radius: number = 0
 
     constructor(maxDistance: number, startAngle: number, startDistance: number){
         this.maxDistance = maxDistance
@@ -60,7 +67,7 @@ class NeonPoint{
         circle( // Filled circle renders better than point
             this.distance * cos(this.angle), 
             this.distance * sin(this.angle),
-            5
+            this.radius
         ) 
         pop()
     }
@@ -70,5 +77,38 @@ class NeonPoint{
 
         const maxNumOfLaps = 30
         this.angle = this.startAngle + random(maxNumOfLaps * TWO_PI * s/100)
+
+        this.radius = map(s, 0, 100, 5, 0)
+    }
+}
+
+
+export class NeonCircle{
+    private readonly radius: number 
+    private readonly blur: number
+    private readonly color: p5.Color
+    private readonly strokeWeight: number
+
+    constructor(radius: number, color: p5.Color, strokeWeight: number, blur: number = 15){
+        this.radius = radius
+        this.color = color
+        this.strokeWeight = strokeWeight
+        this.blur = blur
+    }
+
+    public render(): void{
+        noFill()
+        stroke(this.color)
+        strokeWeight(this.strokeWeight)
+        push()
+        drawingContext.shadowColor = this.color.toString()
+        drawingContext.shadowBlur = this.blur 
+        circle(0, 0, this.radius * 2)
+        pop()
+    }
+
+    public update(s: number): void{
+        const opacity = map(s, 0, 100, 0, 255)
+        this.color.setAlpha(opacity)
     }
 }
