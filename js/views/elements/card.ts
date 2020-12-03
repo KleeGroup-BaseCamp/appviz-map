7e31bf3822434486bb9464ce073e347a8e396c72
phlppchrtn <philippe.chretien@kleegroup.com>
2020-12-04 00:17:04 +0100

    paper

--- js/views/elements/card.ts
+++ js/views/elements/card.ts
@@ -1,3 +1,4 @@
+import {ColorUtils} from "../../utils"
 import {style} from "../../app"
 import {VElement, State} from "../../core"
 import {Header} from "../../components"
@@ -5,10 +6,12 @@ import {PxSize} from "../../layout"
 
 export class Card extends VElement {
     private readonly header: Header
+ //   private readonly paper: Paper
 
     constructor(id: any, pxSize: PxSize, title: string) {
         super(id, pxSize, false)
         this.header = new Header(title, this.getWidth(), 100, style.text.size.xxl)
+ //       this.paper = new Paper(this.getWidth(), this.getHeight(), 50) 
     }
 
     /**
@@ -16,7 +19,9 @@ export class Card extends VElement {
      */
     public render(state :State): void  {
         this.renderBackground()
+   //     this.paper.render(0, 0)
         this.header.render()
+
     }
 
     private renderBackground(): void {
@@ -29,3 +34,26 @@ export class Card extends VElement {
         return false
     }
 }
+
+/*class Paper {
+    private readonly canvas
+    
+    constructor(paperWidth : number, paperHeight : number, arg: number) {
+        //https://www.openprocessing.org/sketch/442874
+        this.canvas = createGraphics(paperWidth, paperHeight)
+//        colorMode(HSB, 100, 100, 100);
+        this.canvas.noStroke()
+        const backColor = ColorUtils.clone(style.color.middle)
+        for (var i = 0; i < paperWidth - 1; i += 2) {
+            for (var j = 0; j < paperHeight - 1; j += 2) {
+                backColor.setAlpha(random(arg))
+                this.canvas.fill(backColor)
+                this.canvas.ellipse(i, j, 2, 2)
+            }
+        }
+    }
+    
+    public render(x:number, y:number) {
+      image(this.canvas, x, y);
+    }
+ }*/
