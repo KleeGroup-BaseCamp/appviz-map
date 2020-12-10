a121925031757003a7af9fa40c78e37ae1dfd7bc
farouk <farouk.faiz@kleegroup.com>
2020-12-10 13:36:57 +0100

    Refact Style

--- ts/neon/components/style.ts
+++ ts/neon/components/style.ts
@@ -9,47 +9,58 @@ export type ComponentsSizes = {
     }
 }
 
-export interface Style {
-    readonly icon:  {  
-        readonly font : p5.Font,
-        readonly size : {
-            readonly s:  number,
-            readonly m:  number,
-            readonly l:  number,
-            readonly xl: number
-        }
-    },
-    readonly text:  {    
-        readonly font : p5.Font,
-        readonly size : {
-            readonly xxs: number,
-            readonly xs: number,
-            readonly s: number,
-            readonly m: number,
-            readonly l: number,
-            readonly xl: number,
-            readonly xxl: number,
-            readonly default: number
-        },
-        readonly color: {
-            readonly primary   : p5.Color, 
-            readonly secondary : p5.Color
-        }
+export interface IconStyle {  
+    readonly font : p5.Font,
+    readonly size : {
+        readonly s:  number,
+        readonly m:  number,
+        readonly l:  number,
+        readonly xl: number
+    }
+}
+
+export interface TextStyle {    
+    readonly font : p5.Font,
+    readonly size : {
+        readonly xxs: number,
+        readonly xs: number,
+        readonly s: number,
+        readonly m: number,
+        readonly l: number,
+        readonly xl: number,
+        readonly xxl: number,
+        readonly default: number
     },
     readonly color: {
-        /* colors */
-        readonly a : p5.Color,
-        readonly b : p5.Color,
-        readonly c : p5.Color,
-        readonly d : p5.Color,
-
-        /* layer > back to front colors */
-        readonly back   : p5.Color, 
-        readonly middle : p5.Color, 
-        readonly front  : p5.Color, 
-
-        readonly undefined :  p5.Color
+        readonly primary   : p5.Color, 
+        readonly secondary : p5.Color
     }
+}
 
+export interface ColorStyle {
+    readonly a : p5.Color,
+    readonly b : p5.Color,
+    readonly c : p5.Color,
+    readonly d : p5.Color,
+
+    /* layer > back to front colors */
+    readonly back   : p5.Color, 
+    readonly middle : p5.Color, 
+    readonly front  : p5.Color, 
+
+    readonly undefined :  p5.Color
+}
+
+export class Style {
+    readonly icon: IconStyle
+    readonly text: TextStyle
+    readonly color: ColorStyle
     readonly pxSizes: ComponentsSizes
+
+    constructor(iconStyle: IconStyle, textStyle: TextStyle, colorStyle: ColorStyle, pxSizes: ComponentsSizes){
+        this.icon = iconStyle
+        this.text = textStyle
+        this.color = colorStyle
+        this.pxSizes = pxSizes
+    }
 }
--- ts/neon/components/styleBuilder.ts
+++ ts/neon/components/styleBuilder.ts
@@ -1,6 +1,7 @@
 import * as p5 from "p5"
 import {ComponentsSizes, Style} from "."
 import {PxSize} from "../layout"
+import { ColorStyle, IconStyle, TextStyle } from "./style"
 
 const su = 10 // Size Unit
 
@@ -96,58 +97,61 @@ export class StyleBuilder {
         if (!theme){
             throw `Theme ${this.themeName} is undefined`
         }
-        return  {
-             icon: {
-                font : this.notNull(this.iconFont), 
-                size : {
-                    s: 16,
-                    m: 20,
-                    l: 26,
-                    xl: 32
-                }
-            },    
-            text : {
-                font : this.notNull(this.textFont),
-                size : {
-                    xxs: 12,
-                    xs: 14,
-                    s: 16,
-                    m: 20,
-                    l: 26,
-                    xl: 32,
-                    xxl: 42,
-                    default: 60
-                },
-                color: {
-                    /* text */
-                    primary : color(theme.text.primary), /* white */ 
-                    secondary : color(theme.text.secondary),  /* light grey */
-                }    
-            },
-            color : {
-                a : color(theme.a), /* blue */
-                b : color(theme.b), /* green */
-                c : color(theme.c), /* red */
-                d : color(theme.d), /* grey */
-        
-                /* back to front colors */
-                back : color(theme.back),  /* deep dark */
-                middle : color(theme.middle),  /* dark */
-                front : color(theme.front),  /* light dark */
-        
-                undefined : color(theme.undefined),  /* lemon*/
-            },
-            pxSizes: this.buildPxSizes() 
-        }
+        return  new Style(
+            this.buildIconStyle(), 
+            this.buildTextStyle(theme), 
+            this.buildColorStyle(theme), 
+            this.buildPxSizes()
+        ) 
     } 
-}
-//        /* LightTheme */
-//        /* back to front colors */
-//        this.color.back   = color("#F6F6F4")  /*  light++ */
-//        this.color.middle  = color("#EFEFEF")  /* light   */
-//        this.color.front   = color("#DDDDDD")  /* light-- */
-//        /* text */
-//        this.text.color.primary   = color("#000000") /* black */
-//        this.text.color.secondary  = color("#615c4b")
-//        this.color.undefined  = color("0008ff")
 
+    private buildIconStyle(): IconStyle{
+        return {
+            font : this.notNull(this.iconFont), 
+            size : {
+                s: 16,
+                m: 20,
+                l: 26,
+                xl: 32
+            }
+        }
+    }
+
+    private buildTextStyle(theme: Theme): TextStyle{
+        return {
+            font : this.notNull(this.textFont),
+            size : {
+                xxs: 12,
+                xs: 14,
+                s: 16,
+                m: 20,
+                l: 26,
+                xl: 32,
+                xxl: 42,
+                default: 60
+            },
+            color: {
+                /* text */
+                primary : color(theme.text.primary), /* white */ 
+                secondary : color(theme.text.secondary),  /* light grey */
+            }    
+        }
+    }
+
+    private buildColorStyle(theme: Theme): ColorStyle{
+        return {
+            a : color(theme.a), /* blue */
+            b : color(theme.b), /* green */
+            c : color(theme.c), /* red */
+            d : color(theme.d), /* grey */
+    
+            /* back to front colors */
+            back : color(theme.back),  /* deep dark */
+            middle : color(theme.middle),  /* dark */
+            front : color(theme.front),  /* light dark */
+    
+            undefined : color(theme.undefined),  /* lemon*/
+        }
+    }
+
+}
